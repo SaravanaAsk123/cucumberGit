a6b5ecf02a9f6a90132915e4e44fdf57292fff7a
SaravanaAsk123 <saravanamechdme@gmail.com>
2022-01-21 00:05:26 +0530

    password

--- src/test/java/org/fb/Sample.java
+++ src/test/java/org/fb/Sample.java
@@ -1,5 +1,14 @@
 package org.fb;
 
 public class Sample {
+	private void password() {
+		System.out.println("ask@123");
+	}
+	public static void main(String[] args) {
+		Sample w = new Sample();
+		w.password();
 
+	
+	}
+	
 }
